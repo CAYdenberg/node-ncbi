ba72ac8b1930a7678ef39821ff4dd81ace5e140f
CAYdenberg <casey@graphos.ca>
2015-10-20 12:32:47 -0600

    Deleted useless parse method

--- Parser/index.js
+++ Parser/index.js
@@ -22,15 +22,6 @@ module.exports = {
     });
   },
 
-  countAndIds: function(data) {
-    var count = this.count(data);
-    var ids = this.ids(data);
-    return {
-      count: count,
-      ids: ids
-    };
-  },
-
   summaries: function(data, single) {
     var dataObj = createDataObj(data);
     return dataObj.find(function(data) {
