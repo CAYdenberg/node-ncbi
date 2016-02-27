ad6baf6f30f0e52c61bb2ceb5ae7fe02e8065365
CAYdenberg <casey@graphos.ca>
2016-02-26 17:02:28 -0700

    Fixed up REPL

--- bin/repl.js
+++ bin/repl.js
@@ -1,17 +1,30 @@
 const repl = require('repl');
-const gateway = require('../src/createGateway');
-const methods = require('../src/methods');
+const open = require('open');
+
 
 var replServer = repl.start({
   prompt: 'node-ncbi> '
 });
 
-replServer.context.gateway = gateway;
-replServer.context.methods = methods;
+const gateway = replServer.context.gateway = require('../src/createGateway');
 
 
-replServer.context.url = function(params) {
+const url = replServer.context.url = function(params) {
   params.test = true;
   var urlGateway = gateway(params);
   return urlGateway.generateUrl();
+}
+
+replServer.context.open = function(params) {
+  open(url(params));
+}
+
+const methodsLib = require('../src/methods');
+
+replServer.context.method = function() {
+  const method = arguments[0];
+  const args = Array.prototype.slice.call(arguments, 1);
+  return methodsLib[method](args).then(result => {
+    console.log(result);
+  });
 }
