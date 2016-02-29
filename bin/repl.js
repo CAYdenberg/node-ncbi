4c09518630eba3fb5d0c73a023d2404a7678e6f7
CAYdenberg <casey@graphos.ca>
2016-02-29 09:25:57 -0700

    REPL doesnt play nice w promises; take out a couple methods

--- bin/repl.js
+++ bin/repl.js
@@ -8,7 +8,6 @@ var replServer = repl.start({
 
 const gateway = replServer.context.gateway = require('../src/createGateway');
 
-
 const url = replServer.context.url = function(params) {
   params.test = true;
   var urlGateway = gateway(params);
@@ -17,14 +16,4 @@ const url = replServer.context.url = function(params) {
 
 replServer.context.open = function(params) {
   open(url(params));
-}
-
-const methodsLib = require('../src/methods');
-
-replServer.context.method = function() {
-  const method = arguments[0];
-  const args = Array.prototype.slice.call(arguments, 1);
-  return methodsLib[method](args).then(result => {
-    console.log(result);
-  });
 }
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-require('./src/methods');
+module.exports = require('./src/methods');
