208af99853dde732189e60672dd4b7be0bb22acf
CAYdenberg <casey@graphos.ca>
2016-03-08 16:31:59 -0700

    Whitespace cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ var eslint = require('gulp-eslint');
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 gulp.task('lint', function() {
-  return gulp.src(['**/*.js','!node_modules/**', '!test/**'])
+  return gulp.src(['**/*.js','!node_modules/**'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
     .pipe(eslint())
     // eslint.format() outputs the lint results to the console.
     // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format());
-})
+});
 
 gulp.task('test', function() {
   return gulp.src(['test/*.js'], { read: false })
