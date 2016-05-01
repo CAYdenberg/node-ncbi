4e54bdc5b06984de72d9bd5425ee5a8259d31c00
Casey Ydenberg <ydenberg@gmail.com>
2016-04-30 21:31:59 -0600

    Rewrote tests for the new interface

--- test/test.js
+++ test/test.js
@@ -17,7 +17,7 @@ describe('Search Gateway', function() {
   var search = createGateway.pubmedSearch('ydenberg ca', 0, 10);
   describe('generateUrl search', function() {
     it('should build a valid search url from parameters', function() {
-      assert.equal(search.generateUrl(), 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=ydenberg%20ca&retstart=0&retmax=10&retmode=json');
+      assert.equal(search.generateUrl(), 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=pubmed&term=ydenberg%20ca&retstart=0&retmax=10');
     });
   });
 
@@ -28,26 +28,29 @@ describe('Links gateway', function() {
   describe('generateUrl links', function() {
     var links = createGateway.pubmedLinks(22588722);
     it('should build a valid link url from parameters', function() {
-      assert.equal(links.generateUrl(), 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?db=pubmed&dbfrom=pubmed&cmd=neighbor&retmode=json&id=22588722');
+      assert.equal(links.generateUrl(), 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?retmode=json&db=pubmed&dbfrom=pubmed&cmd=neighbor&id=22588722');
     });
   });
 
 });
 
+const parse = require('../src/gateways/parse');
 function getDoc(filename, callback) {
+
   var fs = require('fs');
   var path = require('path');
-  fs.readFile(path.join(__dirname, 'docs', filename), 'UTF-8', callback);
+  fs.readFile(path.join(__dirname, 'docs', filename), 'UTF-8', (err, data) => {
+    callback(err, parse(data));
+  });
 }
 
-var createParser = require('../src/documents');
+var q = require('../src/queries');
 describe('parser', function() {
 
   describe('count', function() {
     it('should find the count field in a JSON search result', function(done) {
       getDoc('search.json', function(err, contents) {
-        var parser = createParser(contents, 'esearch');
-        assert.equal(parser.count(), 9);
+        assert.equal(q.count(contents), 9);
         done();
       });
     });
@@ -56,8 +59,7 @@ describe('parser', function() {
   describe('ids', function() {
     it('should return an array of ids', function(done) {
       getDoc('search.json', function(err, contents) {
-        var parser = createParser(contents, 'esearch');
-        assert.equal(parser.ids().length, 9);
+        assert.equal(q.ids(contents).length, 9);
         done();
       })
     });
@@ -66,9 +68,7 @@ describe('parser', function() {
   describe('summary', function() {
     it('should find all of the info for each summary in the result set', function(done) {
       getDoc('summary.json', function(err, contents) {
-        var parser = createParser(contents, 'esummary');
-        var summaries = parser.summaries();
-        assert.equal(summaries.length, 9);
+        assert.equal(q.summaries(contents).length, 9);
         done();
       });
     });
@@ -77,8 +77,7 @@ describe('parser', function() {
   describe('abstract', function() {
     it('should retrieve the abstract from an xml string', function(done) {
       getDoc('fetch.xml', function(err, contents) {
-        var parser = createParser(contents, 'efetch');
-        assert.ok( parser.abstracts(true) );
+        assert.ok(q.abstract(contents));
         done();
       });
     });
@@ -87,8 +86,7 @@ describe('parser', function() {
   describe('citedBy', function() {
     it('should find all of the papers that have cited this one', function(done) {
       getDoc('elink.json', function(err, contents) {
-        var parser = createParser(contents, 'elink');
-        assert.equal(parser.citedBy().length, 2);
+        assert.equal(q.citedBy(contents).length, 2);
         done();
       });
     })
@@ -97,8 +95,7 @@ describe('parser', function() {
   describe('cites', function() {
     it('should find all the papers this paper cites', function(done) {
       getDoc('elink.json', function(err, contents) {
-        var parser = createParser(contents, 'elink');
-        assert.ok(parser.cites());
+        assert.ok(q.cites(contents));
         done();
       });
     });
@@ -107,8 +104,7 @@ describe('parser', function() {
   describe('similar', function() {
     it('should find all the papers PubMed flags as similar to this one', function(done) {
       getDoc('elink.json', function(err, contents) {
-        var parser = createParser(contents, 'elink');
-        assert.ok(parser.similar());
+        assert.ok(q.similar(contents));
         done();
       });
     });
