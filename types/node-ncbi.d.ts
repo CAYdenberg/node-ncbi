d3436c739bc78d567d86f5c71c3711582a354a09
Casey Ydenberg <hi@caydenberg.io>
2022-11-16 16:07:58 -0800

    Add typescript types

---
+++ types/node-ncbi.d.ts
@@ -0,0 +1,36 @@
+declare module "node-ncbi" {
+  module pubmed {
+    interface Paper {
+      raw: object;
+      pubDate: string;
+      title: string;
+      authors: string;
+      pmid: number;
+      pmc?: number;
+      doi?: string;
+    }
+
+    function search(
+      term: string,
+      skip?: number,
+      limit?: number
+    ): Promise<{
+      count: number;
+      papers: Array<Paper>;
+    }>;
+
+    function summary(pmid: number): Promise<Paper>;
+
+    function abstract(pmid: number): Promise<string>;
+
+    function cites(pmid: number): Promise<number[]>;
+
+    function citedBy(pmid: number): Promise<number[]>;
+
+    function similar(pmid: number): Promise<number[]>;
+
+    function isOa(pmid: number): Promise<boolean>;
+
+    function fullText(pmid: number): Promise<string>;
+  }
+}
